84b9aeeb21a514f82a3dcd173119126613c0f345
Soumya Ghosh <soumya.ghosh71@gmail.com>
2023-04-29 20:48:11 +0530

    removed extra imports

--- src/main/java/com/example/crudbasics/CrudbasicsApplication.java
+++ src/main/java/com/example/crudbasics/CrudbasicsApplication.java
@@ -1,9 +1,5 @@
 package com.example.crudbasics;
 
-import com.example.crudbasics.model.Student;
-import com.example.crudbasics.repository.StudentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
